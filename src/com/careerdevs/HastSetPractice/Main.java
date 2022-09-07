3399ad8c9286ac83bb3b53d61cd15a7881401273
MayraMendoza <mayra601m@gmail.com>
2022-09-07 19:58:38 -0400

    Initial commit

---
+++ src/com/careerdevs/HastSetPractice/Main.java
@@ -0,0 +1,31 @@
+package com.careerdevs.HastSetPractice;
+
+import com.careerdevs.HastSetPractice.models.User;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    User one = new User("Mayra601m");
+
+    User two = new User("Mayra601m");
+
+    Set<User> userNameSet= new HashSet<>();
+
+    userNameSet.add(one);
+    userNameSet.add(two);
+
+    System.out.println(userNameSet.contains(two.getUserName()));
+    System.out.println(two.getUserName());
+
+
+    System.out.print(userNameSet);
+
+
+    }
+}
---
+++ src/com/careerdevs/HastSetPractice/models/User.java
@@ -0,0 +1,43 @@
+package com.careerdevs.HastSetPractice.models;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class User {
+    String userName;
+//    Set<String> userName;
+//    String Password;
+
+
+    public User(String userName) {
+        this.userName = userName;
+    }
+
+    public User() {
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "\"userName\":\"" + userName + '"' +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
